4e008282bc507c27816a3769364fe21bd9160fe2
Sam Zipper <samuelczipper@gmail.com>
2017-02-03 14:27:56 -0500

    Update met input data to vary through time

--- 1D/WierengaEtAl_1991/ProcessingScripts/CreateMetInputData.R
+++ 1D/WierengaEtAl_1991/ProcessingScripts/CreateMetInputData.R
@@ -19,21 +19,28 @@ rm(list=ls())
 require(lubridate)
 
 # path to save output met file
-out.path <- "C:/Users/Sam/src/geotop_examples/1D/WierengaEtAl_1991/meteo/meteo0001.txt"
+out.path <- "C:/Users/Sam/src/geotop_examples/1D/WierengaEtAl_1991/meteo/meteotrench0001.txt"
 
 # start and end datetime
 date.start <- dmy_hm("27/05/1987 00:00")
 date.end <- dmy_hm("21/08/1987 23:00")
+dates.all <- seq(date.start, date.end, by="hour")
 
 # make data frame
-df.met <- data.frame(Date = format(seq(date.start, date.end, by="hour"), "%d/%m/%Y %H:%M"),
-                     Iprec = sprintf("%f", 1.82*10/24), 
-                     WindSp = sprintf("%f", 0),
-                     WindDir = sprintf("%f", 90.0),
-                     RH = sprintf("%f", 100.0),
-                     AirT = sprintf("%f", 5.0),
-                     Swglob = sprintf("%f", 0.0),
-                     CloudTrans = sprintf("%f", 1.0))
+df.met <- data.frame(Date = format(dates.all, "%d/%m/%Y %H:%M"),
+                     Iprec = sprintf("%f", 1.82*10/24),
+                     WindSp = rnorm(n=length(dates.all), mean=10, sd=0.1),
+                     WindDir = rnorm(n=length(dates.all), mean=20, sd=0.1),
+                     RH = rnorm(n=length(dates.all), mean=90, sd=0.1),
+                     AirT = rnorm(n=length(dates.all), mean=5, sd=0.1),
+                     Swglob = rnorm(n=length(dates.all), mean=30, sd=0.1),
+                     CloudTrans = rnorm(n=length(dates.all), mean=0.9, sd=0.01))
+                     # WindSp = sprintf("%f", 0),
+                     # WindDir = sprintf("%f", 90.0),
+                     # RH = sprintf("%f", 100.0),
+                     # AirT = sprintf("%f", 5.0),
+                     # Swglob = sprintf("%f", 0.0),
+                     # CloudTrans = sprintf("%f", 1.0))
 
 # save output file
 write.csv(df.met, out.path, quote=F, row.names=F)
